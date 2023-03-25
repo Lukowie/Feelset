f65e0a8ebb85919419935c836debe53c7259865e
Lukowie <lukevdm64@gmail.com>
2023-03-25 13:49:32 -0400

    added db query line, pushing so it can be used on server instance

--- pages/api/formHandler.js
+++ pages/api/formHandler.js
@@ -1,5 +1,20 @@
+import executeQuery from "../../lib/db";
+
 export default function formHandler(req, res) {
     const body = req.body;
+
+    const todayNow = new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+    const query = 'INSERT INTO submissions (ArtistName, Linktree, Soundcloud, Spotify, Instagram, Twitter, SongName, SongPath, DateSubmitted) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    const values = [body.artistName, body.linktree, body.soundcloud, body.spotify, body.instagram, body.twitter, body.songName, body.songPath, todayNow];
+
+    const results = executeQuery({query, values});
+
+    if (results.error) {
+        console.log(results.error);
+    } else {
+        console.log('DAte inserted successfully!');
+    }
     res.status(200).json(
         {
             data: 
